52d9c601f1bef6702c484f474e523c99cdde96f5
hgedia <hgedia@hgedia.co>
2018-11-14 14:00:00 +0000

    .

--- bundle.js
+++ bundle.js
@@ -7150,61 +7150,77 @@ ethjsPersonalSignButton.addEventListener('click', function(event) {
 })
 
 
-signTypedDataButton.addEventListener('click', function(event) {
-  event.preventDefault()
+  signTypedDataButton.addEventListener('click', function (event) {
+    event.preventDefault()
 
-  const msgParams = [
-    {
-      type: 'string',
-      name: 'Message',
-      value: 'Hi, Bobbo!'
-    },
-    {
-      type: 'uint32',
-      name: 'A number',
-      value: '1337'
-    }
-  ]
+    var signer = web3.eth.accounts[0]
+    if (!signer) return connect()
+    console.log('CLICKED, SENDING SignedType Data SIGN REQ')
 
-  var from = web3.eth.accounts[0]
+    const domain = [
+      { name: "name", type: "string" },
+      { name: "version", type: "string" },
+      { name: "chainId", type: "uint256" },
+      { name: "verifyingContract", type: "address" },
+      { name: "salt", type: "bytes32" },
+    ];
+    const bid = [
+      { name: "amount", type: "uint256" },
+      { name: "bidder", type: "Identity" },
+    ];
+    const identity = [
+      { name: "userId", type: "uint256" },
+      { name: "wallet", type: "address" },
+    ];
 
-  /*  web3.eth.signTypedData not yet implemented!!!
-   *  We're going to have to assemble the tx manually!
-   *  This is what it would probably look like, though:
-    web3.eth.signTypedData(msg, from) function (err, result) {
-      if (err) return console.error(err)
-      console.log('PERSONAL SIGNED:' + result)
-    })
-  */
+    const domainData = {
+      name: "My amazing dApp",
+      version: "2",
+      chainId: parseInt(web3.version.network, 10),
+      verifyingContract: "0x1C56346CD2A2Bf3202F771f50d3D14a367B48070",
+      salt: "0xf2d857f4a3edcb9b78b4d503bfe733db1e3f6cdc2b7971ee739626c97e86a558"
+    };
+    var message = {
+      amount: 100,
+      bidder: {
+        userId: 323,
+        wallet: "0x3333333333333333333333333333333333333333"
+      }
+    };
 
-   console.log('CLICKED, SENDING PERSONAL SIGN REQ')
-  var params = [msgParams, from]
-  console.dir(params)
-  var method = 'eth_signTypedData'
+    const data = JSON.stringify({
+      types: {
+        EIP712Domain: domain,
+        Bid: bid,
+        Identity: identity,
+      },
+      domain: domainData,
+      primaryType: "Bid",
+      message: message
+    });
 
-  web3.currentProvider.sendAsync({
-    method,
-    params,
-    from,
-  }, function (err, result) {
-    if (err) return console.dir(err)
-    if (result.error) {
-      alert(result.error.message)
-    }
-    if (result.error) return console.error(result)
-    console.log('PERSONAL SIGNED:' + JSON.stringify(result.result))
 
-    const recovered = sigUtil.recoverTypedSignature({ data: msgParams, sig: result.result })
 
-    if (recovered === from ) {
-      alert('Successfully ecRecovered signer as ' + from)
-    } else {
-      alert('Failed to verify signer when comparing ' + result + ' to ' + from)
-    }
+    web3.currentProvider.sendAsync(
+      {
+        method: "eth_signTypedData_v3",
+        params: [signer, data],
+        from: signer
+      },
+      function (err, result) {
+        if (err) {
+          return console.error(err);
+        }
+        const signature = result.result.substring(2);
+        const r = "0x" + signature.substring(0, 64);
+        const s = "0x" + signature.substring(64, 128);
+        const v = parseInt(signature.substring(128, 130), 16);
+        // The signature is now comprised of r, s, and v.
+        console.log("Signature is " + signature)
+      }
+    );
 
   })
-
-})
 
 ethjsSignTypedDataButton.addEventListener('click', function(event) {
   event.preventDefault()
